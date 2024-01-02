a099261650db92fa43dade6d181a17439bda34d8
Jerry Tom <112073612+Jerry2T@users.noreply.github.com>
2024-01-02 13:00:55 +0000

    N

---
+++ programs/java/jv3.java
@@ -0,0 +1,15 @@
+public class jv3{
+    public static void main(String[] args) {
+        int x=3;
+        switch (7) {
+            case 1:System.out.println("hi");                
+                break;            
+            case 2:System.out.println("hi2");  
+                break;
+            case 3:System.out.println("hibye");  
+                break;   
+            default:System.out.println("hi000000kjhdb");  
+                break;
+        }
+    }
+}
---
+++ programs/java/jv4.java
@@ -0,0 +1,15 @@
+public class jv4 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        //int i = 0;
+do {
+  System.out.println(i);
+  i++;
+}
+while (i < 5);
+    }    
+}
