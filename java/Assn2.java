1537d769a75daa801d41ef64b269b5c132b78b3b
Jerry Tom <112073612+Jerry2T@users.noreply.github.com>
2024-01-26 17:02:53 +0000

    .

---
+++ java/Assn2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Assn2 {
+    public static void main(String arr[]){
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.print("Enter no.1: ");
+        long ano=sc.nextInt();
+
+        System.out.print("Enter no.2: ");
+        long bno=sc.nextInt();
+
+        long res=multi(ano,bno);
+        long result=adds(res);
+        System.out.println("Final ans: "+result);
+    }
+
+    public static long multi(long a,long b){
+        long ans=1;
+        for(long i=a;i<b;i++){
+            ans*=i;
+        }
+        return ans;
+    }
+
+    public static int adds(long a){
+        int sum=0;
+        while(a>0){
+            sum+=a%10;
+            a/=10;
+        }
+        while(a>=10){
+            sum=adds(sum);
+        }
+        return sum;
+    }
+}
---
+++ java/Assn3.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Assn3 {
+    public static void main(String[] args) {
+            
+    }    
+}
