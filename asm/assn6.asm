6b2a6fc77172f95ee3b304b8a37242318fc11010
Jerry Tom <112073612+Jerry2T@users.noreply.github.com>
2024-03-24 17:25:54 +0000

    .

--- asm/assn6.asm
+++ asm/assn6.asm
@@ -1,77 +1,111 @@
 section .data
-bmsg db 10,"Before Transfer: "
-bmsg_len equ $-bmsg
+    bmsg db 10,"Before Transfer: "
+    bmsg_len equ $-bmsg
+    amsg db 10,"After Transfer: "
+    amsg_len equ $-amsg
+    smsg db 10,"Source block: "
+    smsg_len equ $-smsg
+    dmsg db 10,"Destination block: "
+    dmsg_len equ $-dmsg
+    sblock  db 11h,22h,33h,44h,55h
+    dblock times 5 db 0
 
-amsg db 10,"After Transfer: "
-amsg_len equ $-amsg
-
-smsg db 10,"Source block: "
-smsg_len equ $-smsg
-
-dmsg db 10,"Destination block: "
-dmsg_len equ $-dmsg
-
-sblock  db 11h,22h,33h,44h,55h
-dblock times 5 db 0
 section .bss
-char_ans resb 2
+    char_ans resb 2
+
+%macro print 2
+	mov rax,1
+	mov rdi,1
+	mov rsi,%1
+	mov rdx,%2
+	syscall
+%endmacro
+
+%macro read 2
+	mov rax,0
+	mov rdi,0
+	mov rsi,%1
+	mov rdx,%2
+	syscall
+%endmacro
+
+%macro exit 0
+	mov rax,60
+	mov rdi,0
+	syscall
+%endmacro
 
 
 section .text
-global _start
+    global _start
+_start:
+    Print bmsg,bmsg_len
+    Print smsg,smsg_len
+    mov rsi,sblock
+    call disp_block
 
-start:
-Print bmsg,bmsg_len
+    Print dmsg,dmsg_len
+    mov rsi,sblock
+    call disp_block
 
-Print smsg,smsg_len
-mov rsi,sblock
-call disp_block
+    call BT_NO
+    Print bmsg,bmsg_len
 
-Print dmsg,dmsg_len
-mov rsi,sblock
-call disp_block
+    Print smsg,smsg_len
+    mov rsi,sblock
+    call disp_block
 
-call BT_NO
+    Print dmsg,dmsg_len
+    mov rsi,sblock
+    call disp_block
 
-Print bmsg,bmsg_len
+    BT_NO:
+        mov rsi,sblock
+        mov dsi,dblock
+        mov rcx,5
 
-Print smsg,smsg_len
-mov rsi,sblock
-call disp_block
+    back:   
+        mov al,[rsi]
+        mov [rdi],al
+        inc rsi
+        inc rdi
+        dec rcx
+        jnz back
+        ret
 
-Print dmsg,dmsg_len
-mov rsi,sblock
-call disp_block
+    disp_block:
+        mov rbp,5
+    next_num:
+        mov al,[rsi]
+        push rsi
 
-BT_NO:
-mov rsi,sblock
-mov dsi,dblock
-mov rcx,5
+        call disp_8
+        Print space,1
+        pop rsi
+        inc rsi
 
-back:   mov al,[rsi]
-mov [rdi],al
-inc rsi
-inc rdi
-dec rcx
-jnz back
-ret
+        dec rbp
+        jnz next_num
+        ret
+    
+disp_8:
+    mov rbx,16
+    mov rcx,2
+    mov rsi,charans+1
 
-disp_block:
-mov rbp,5
+cnt:
+    mov rdx,0
+    div rbx 
 
-next_num:
-mov al,[rsi]
-push rsi
+    cmp dl,09h
+    jbe add30
+    add dl,07h
+add30: add dl,30h
 
-call disp_8
-Print space,1
+    mov [rsi],dl
+    dec rsi
+    dec rcx
+    jnz cnt
 
-pop rsi
-inc rsi
-
-dec rbp
-jnz next_num
- ret
- 
- disp_8:
-  mov rsi,char_ans+1
+    Print charans,2
+    ret
